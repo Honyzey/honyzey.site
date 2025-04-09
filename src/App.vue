42241cf2c59852dac665decf13733fcb3241174e
Honyzey <briac35.v@gmail.com>
2025-04-09 16:20:43 +0200

    update server config

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
 
-  <div class="app-container">
+  <div>
 
-    <Header/>
+    <SplashScreen v-if="showSplash" @dismissed="hideSplash" />
 
-    <main class="content">
+    <div class="app-container" :class="{ 'app-visible': !showSplash }">
 
-      <h1>Mon portfolio</h1>
+      <Header/>
 
-    </main>
+      <main class="content">
+      
+        <h1>Mon portfolio</h1>
+      
+      </main>
 
-    <Footer/>
+      <Footer/>
+
+    </div>
 
   </div>
+  
  
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Footer from './components/Footer.vue';
+import SplashScreen from './components/SplashScreen.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Footer
+    Footer,
+    SplashScreen
+  },
+
+  data() {
+    return {
+      showSplash: true
+    }
+  },
+
+  methods: {
+    hideSplash() {
+      this.showSplash = false;
+    }
   }
 };
 </script>
@@ -36,10 +57,19 @@ export default {
   min-height: 100vh;
 }
 
+.app-visible {
+  opacity: 1;
+  transform: scale(1);
+}
+
 .content {
   flex: 1;
   width: 90%;
   margin: 0 auto;
   padding: 20px 0;
+}
+
+body.no-scroll {
+  overflow: hidden;
 }
 </style>
